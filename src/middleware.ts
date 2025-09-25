49b67d2708881e81b394b436c0ba6a4b4c01e97b
Anıl Soylu <anil.soylu94@gmail.com>
2025-09-25 19:45:40 +0300

    added no robots

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,36 +2,44 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { ALLOWED_IPS } from "./lib/ip_list";
 
+const isAllowedIP = (ipAddress: string) => {
+  for (const allowedIP of ALLOWED_IPS) {
+    if (allowedIP.endsWith(".")) {
+      if (ipAddress.startsWith(allowedIP)) return true;
+    } else if (ipAddress === allowedIP) return true;
+  }
+  return false;
+};
+
 export function middleware(request: NextRequest) {
   const ip =
     request.headers.get("x-forwarded-for")?.split(",")[0] ||
     request.headers.get("x-real-ip") ||
     "";
 
-  const userAgent = request.headers.get("user-agent")?.toLowerCase() || "";
-  const referer = request.headers.get("referer") || "";
+  const userAgent = request.headers.get("user-agent") || "";
   const response = NextResponse.next();
 
-  const isAllowedIP = (ipAddress: string) => {
-    if (ALLOWED_IPS.includes(ipAddress)) {
-      return true;
-    }
-    return ALLOWED_IPS.some(
-      (allowedIP) => ipAddress.startsWith(allowedIP) && allowedIP.endsWith(".")
-    );
-  };
-
-  const isMobile =
-    userAgent.includes("android") || userAgent.includes("iphone");
-  const isFromGoogle =
-    referer.includes("google.com") || referer.includes("google.com.tr");
+  const isGoogleBot = userAgent.includes("Googlebot");
 
   let layoutType = "fakeui";
 
-  if (isAllowedIP(ip)) {
+  if (isAllowedIP(ip) && isGoogleBot) {
     layoutType = "default";
-  } else if (isFromGoogle && isMobile) {
-    layoutType = "adsui";
+  } else {
+    const userAgentLower = userAgent.toLowerCase();
+    const isMobile =
+      userAgentLower.includes("android") || userAgentLower.includes("iphone");
+
+    if (isMobile) {
+      const referer = request.headers.get("referer") || "";
+      const isFromGoogle =
+        referer.includes("google.com") || referer.includes("google.com.tr");
+
+      if (isFromGoogle) {
+        layoutType = "adsui";
+      }
+    }
   }
 
   response.headers.set("x-layout-type", layoutType);
@@ -40,5 +48,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|hreflang.xml).*)"],
 };
